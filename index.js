db3bf14d9213f046b2d52e2d1f7fe003c8ad7e22
Eva Fridman <fridman.1999@mail.ru>
2026-07-03 17:01:56 +0300

    feat: add start page, changes table and hatespeech regex

---
+++ index.js
@@ -0,0 +1,36 @@
+const main = document.querySelector("main");
+const textarea = document.querySelector("textarea");
+const makeTranslitButton = document.querySelector(".make-translit");
+const clearButton = document.querySelector(".clear");
+const outputList = document.querySelector(".output-list");
+const li = document.querySelector("li");
+const originalText = document.querySelector(".original-text");
+const translitText = document.querySelector(".translit-text");
+const deleteButton = document.querySelector(".delete");
+
+
+// Таблица замен для транслитерации
+const translitTable = {
+    'а': 'a', 'б': 'b', 'в': 'v', 'г': 'g', 'д': 'd',
+    'е': 'e', 'ё': 'yo', 'ж': 'zh', 'з': 'z', 'и': 'i',
+    'й': 'y', 'к': 'k', 'л': 'l', 'м': 'm', 'н': 'n',
+    'о': 'o', 'п': 'p', 'р': 'r', 'с': 's', 'т': 't',
+    'у': 'u', 'ф': 'f', 'х': 'kh', 'ц': 'ts', 'ч': 'ch',
+    'ш': 'sh', 'щ': 'shch', 'ъ': '', 'ы': 'y', 'ь': '\'',
+    'э': 'e', 'ю': 'yu', 'я': 'ya'
+};
+
+// Регулярное выражение для фильтрации мата
+const hateSpeech = /(?<![а-яё])(?:(?:(?:у|[нз]а|(?:хитро|не)?вз?[ыьъ]|с[ьъ]|(?:и|ра)[зс]ъ?|(?:о[тб]|п[оа]д)[ьъ]?|(?:\S(?=[а-яё]))+?[оаеи-])-?)?(?:[её](?:б(?!о[рй]|рач)|п[уа](?:ц|тс))|и[пб][ае][тцд][ьъ]).*?|(?:(?:н[иеа]|(?:ра|и)[зс]|[зд]?[ао](?:т|дн[оа])?|с(?:м[еи])?|а[пб]ч|в[ъы]?|пр[еи])-?)?ху(?:[яйиеёю]|л+и(?!ган)).*?|бл(?:[эя]|еа?)(?:[дт][ьъ]?)?|\S*?(?:п(?:[иеё]зд|ид[аое]?р|ед(?:р(?!о)|[аое]р|ик)|охую)|бля(?:[дбц]|тс)|[ое]ху[яйиеё]|хуйн).*?|(?:о[тб]?|про|на|вы)?м(?:анд(?:[ауеыи](?:л(?:и[сзщ])?[ауеиы])?|ой|[ао]в.*?|юк(?:ов|[ауи])?|е[нт]ь|ища)|уд(?:[яаиое].+?|е?н(?:[ьюия]|ей))|[ао]л[ао]ф[ьъ](?:[яиюе]|[еёо]й))|елд[ауые].*?|ля[тд]ь|(?:[нз]а|по)х)(?![а-яё])/;
+
+// Стартовое состояние страницы
+function renderPage () {
+    textarea.value = "Собака сутулая";
+    originalText.textContent = "Собака сутулая";
+    translitText.textContent = "Sobaka sutulaya"
+}
+renderPage();
+
+// Валидация ввода
+
+// Удаление строк
